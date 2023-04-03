e847b2b40ab87e0caaaf43628c25ce985cfed15a
Sankaran Vaidyanathan <sankaranv@cs.umass.edu>
2023-04-03 16:49:51 -0400

    Add example for analytical marginalization with funsor

--- funsor_example.py
+++ funsor_example.py
@@ -1,94 +1,107 @@
-from collections import OrderedDict
+import argparse
 
 import torch
+import sys
 import funsor
-from funsor.domains import reals, bint
-from funsor.gaussian import Gaussian
-from funsor.terms import Variable
-from funsor.torch import Tensor
-from funsor.ops import add, logaddexp
+import funsor.ops as ops
+import funsor.torch.distributions as dist
+from funsor.interpreter import reinterpret
+from funsor.optimizer import apply_optimizer
 
-import pyro.distributions as dist
-from pyro.distributions import constraints
-import pyro
 
-from matplotlib import pyplot as plt
+def kalman_filter(args):
+    funsor.set_backend("torch")
 
-def gaussian_mixture_model(x, i_z, prec_z, i_x, prec_x):
-    x = Tensor(x, OrderedDict(), 'real')
-    z = Variable('z', reals(2,3))
-    c = Variable('c', bint(2))
-    j = Variable('j', bint(50))
-    logp_c = Tensor(torch.tensor([0.5,0.5]).log(), OrderedDict(c = bint(2)))
-    logp_z = Gaussian(i_z, prec_z, OrderedDict(z = reals(3)))
-    logp_xc = Gaussian(i_x, prec_x, OrderedDict(c = bint(2), z = reals(3), x = reals(3)))
+    # Declare parameters.
+    trans_noise = torch.tensor(0.1, requires_grad=True)
+    emit_noise = torch.tensor(0.5, requires_grad=True)
+    params = [trans_noise, emit_noise]
 
-    logp_x = logp_c + logp_xc(z = z[c], x = x[j])
-    logp_x = logp_x.reduce(logaddexp, 'c').reduce(add, 'j')
-    logp_x = logp_x + logp_z(z = z[c]).reduce(add, 'c')
-    logp_x = logp_x.reduce(logaddexp, 'z')
-    return logp_x
+    # A Gaussian HMM model.
+    def model(data):
+        log_prob = funsor.to_funsor(0.0)
 
-def marginalize_gaussian_model(sigma_sq = 1):
+        x_curr = funsor.Tensor(torch.tensor(0.0))
+        for t, y in enumerate(data):
+            x_prev = x_curr
+            # print(y)
+            # A delayed sample statement.
+            x_curr = funsor.Variable("x_{}".format(t), funsor.Real)
+            val1= dist.Normal(1 + x_prev / 2.0, trans_noise, value=x_curr)
+            print(val1)
+            log_prob += val1
+            # print(val1)
+            # print(log_prob)
 
-    x = Gaussian(0, sigma_sq)
+            # Optionally marginalize out the previous state.
+            if t > 0 and not args.lazy:
+                log_prob = log_prob.reduce(ops.logaddexp, x_prev.name)
 
-def generate_data():
+            # An observe statement.
+            val2 = dist.Normal(0.5 + 3 * x_curr, emit_noise, value=y)
+            log_prob += val2
+            # print(log_prob.inputs)
+            # print(val2)
+            # print(log_prob)
+            # sys.exit()
 
-    n = 1000
-    x = torch.randn(n,1)
-    sigma = 4
-    mu = 2
-    x = x * sigma + mu
-    return x
+        # Marginalize out all remaining delayed variables.
+        log_prob = log_prob.reduce(ops.logaddexp)
+        return log_prob
 
-def model(params):
-    mu = pyro.sample('mu', dist.Normal(params[0], params[1]))
-    sigma = pyro.sample('sigma', dist.Gamma(torch.abs(params[2]), torch.abs(params[3])))
-    return pyro.sample('x', dist.Normal(mu, sigma))
+    # Train model parameters.
+    torch.manual_seed(0)
+    data = torch.randn(args.time_steps)
+    optim = torch.optim.Adam(params, lr=args.learning_rate)
+    for step in range(args.train_steps):
+        optim.zero_grad()
+        if args.lazy:
+            with funsor.interpretations.lazy:
+                log_prob = apply_optimizer(model(data))   
+            log_prob = reinterpret(log_prob)
+        else:
+            log_prob = model(data)
+        assert not log_prob.inputs, "free variables remain"
+        loss = -log_prob.data
+        loss.backward()
+        optim.step()
+        if args.verbose and step % 10 == 0:
+            print("step {} loss = {}".format(step, loss.item()))
 
-def guide(params):
-    mu_prior_mean = pyro.param('mu_prior_mean', params[0])
-    mu_prior_std = pyro.param('mu_prior_std', params[1], constraint=constraints.positive)
-    sigma_prior_a = pyro.param('sigma_prior_a', params[2], constraint=constraints.positive)
-    sigma_prior_b = pyro.param('sigma_prior_b', params[3], constraint=constraints.positive)
+def gaussian_funsor_elim():
 
-    mu = pyro.sample('mu', dist.Normal(mu_prior_mean, mu_prior_std))
-    sigma = pyro.sample('sigma', dist.Gamma(sigma_prior_a, sigma_prior_b))
-    x = pyro.sample('x', dist.Normal(mu, sigma))
-    return x
+    x1 = funsor.Variable("x_1", funsor.Real)
+    x2 = funsor.Variable("x_2", funsor.Real)
+    x3 = funsor.Variable("x_3", funsor.Real)
+
+    # Prior over x1
+    mu = funsor.Tensor(torch.tensor(0.0))
+    sigma = torch.tensor(1., requires_grad=True)
+
+    # Build joint over x1, x2, x3
+    joint_log_prob = funsor.to_funsor(0.0)
+    joint_log_prob += dist.Normal(mu, sigma, value=x1)
+    joint_log_prob += dist.Normal(x1, sigma, value=x2)
+    joint_log_prob += dist.Normal(x2, sigma, value=x3)
+
+    # Marginalize
+    marginalized_vars = set([x2.name])
+    joint_log_prob = joint_log_prob.reduce(ops.logaddexp, reduced_vars=marginalized_vars)
+    return joint_log_prob
 
 if __name__ == "__main__":
 
-    # marginalize_gaussian_model()
-    x = generate_data()
-    conditional_model = pyro.condition(model, data = {'x': x.flatten()})
+    with funsor.interpretations.lazy:
+        joint_log_prob = gaussian_funsor_elim()
+        print("Inputs: ", joint_log_prob.inputs)
+        print("Joint Dist: ", joint_log_prob)
 
-    svi = pyro.infer.SVI(model=conditional_model,
-                        guide=guide,
-                        optim=pyro.optim.SGD({"lr": 0.00001, "momentum": 0.8}),
-                        loss=pyro.infer.Trace_ELBO(),
-                        )
-    
-    prior_params = torch.Tensor([0.0, 10.0, 1.0, 0.1])
-    losses, mu_prior_mean_samples, mu_prior_std_samples, sigma_prior_a_samples, sigma_prior_b_samples = [], [], [], [], []
-    pyro.clear_param_store()
-    num_iterations = 5000
-
-    for i in range(num_iterations):
-        loss = svi.step(prior_params)
-        losses.append(loss)
-        mu_prior_mean_samples.append(pyro.param('mu_prior_mean').item())
-        mu_prior_std_samples.append(pyro.param('mu_prior_std').item())
-        sigma_prior_a_samples.append(pyro.param('sigma_prior_a').item())
-        sigma_prior_b_samples.append(pyro.param('sigma_prior_b').item())
-
-    # plt.figure(figsize=(12, 8))
-    plt.plot(losses)
-    plt.xlabel('iteration')
-    plt.ylabel('loss')
-    plt.title('ELBO')
-    plt.show()
-
-
-
+    # parser = argparse.ArgumentParser(description="Kalman filter example")
+    # parser.add_argument("-t", "--time-steps", default=10, type=int)
+    # parser.add_argument("-n", "--train-steps", default=101, type=int)
+    # parser.add_argument("-lr", "--learning-rate", default=0.05, type=float)
+    # parser.add_argument("--lazy", action="store_false")
+    # parser.add_argument("--filter", action="store_false")
+    # parser.add_argument("-v", "--verbose", action="store_true")
+    # args = parser.parse_args()
+    # kalman_filter(args)
